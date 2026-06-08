c21575f610e089d636ace412961bcbebfcf41ed3
singhambuja02-ai <singhambuja02@gmail.com>
2026-06-08 16:33:14 +0530

    Time: 4 ms (31.84%) | Memory: 8.5 MB (91.81%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution { 
+    public :
+    //  palindrome!
+    bool isPalindrome(int x) {
+        if (x < 0 || (x%10 == 0 && x!=0))
+        { return false ;}
+        long long reversed = 0;
+        int original = x;
+        while (x>0) {
+            reversed = reversed *10 + (x%10);
+            x/=10;
+        }
+        return original ==(int)reversed;
+    }
+};
+    

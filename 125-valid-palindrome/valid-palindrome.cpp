0b4aeffe98c8212675e9e8681b85c518d53f030a
singhambuja02-ai <singhambuja02@gmail.com>
2026-06-10 19:16:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.23%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            // Skip non-alphanumeric characters from the left
+            while (left < right && !std::isalnum(s[left])) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right
+            while (left < right && !std::isalnum(s[right])) {
+                right--;
+            }
+            
+            // Compare characters by converting them to lowercase
+            if (std::tolower(s[left]) != std::tolower(s[right])) {
+                return false;
+            }
+            
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};

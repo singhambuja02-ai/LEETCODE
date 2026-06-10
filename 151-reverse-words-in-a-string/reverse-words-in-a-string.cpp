44308cfddc5a6fd66e7f12ebb98c076d74bb861c
singhambuja02-ai <singhambuja02@gmail.com>
2026-06-10 22:25:08 +0530

    Time: 7 ms (14.89%) | Memory: 23.7 MB (9.89%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,9 +4,7 @@ public:
     string reverseWords(string s) {
         stringstream ss(s);
         string word;
-        string result = "";
-        
-        // Extract words and prepend them to the result string
+        string result="";
         while (ss >> word) {
             if (result.empty()) {
                 result = word;

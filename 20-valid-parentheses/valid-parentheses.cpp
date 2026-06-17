4022613afeaf6b25b25fb662ae6131c996bfe8ea
singhambuja02-ai <singhambuja02@gmail.com>
2026-06-17 20:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (67.08%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+    bool isValid(std::string s) {
+        std::stack<char> st;
+        
+        for (char c : s) {
+            
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            
+            else {
+                
+                if (st.empty()) return false;
+                
+                
+                if ((c == ')' && st.top() == '(') ||
+                    (c == '}' && st.top() == '{') ||
+                    (c == ']' && st.top() == '[')) {
+                    st.pop(); 
+                } else {
+                    return false; // Mismatched brackets
+                }
+            }
+        }
+        
+        
+        return st.empty();
+    }
+};
